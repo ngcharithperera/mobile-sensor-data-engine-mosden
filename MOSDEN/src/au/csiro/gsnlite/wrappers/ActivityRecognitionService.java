75182daf3324c04302ccc51476e1bf93c7559405
Charith Perera <ngcharithperera@gmail.com>
2015-09-03 11:12:51 +0100

    no message

--- MOSDEN/src/au/csiro/gsnlite/wrappers/ActivityRecognitionService.java
+++ MOSDEN/src/au/csiro/gsnlite/wrappers/ActivityRecognitionService.java
@@ -27,21 +27,22 @@ public class ActivityRecognitionService extends IntentService	 {
 		}
 	}
 	
-//	private String getType(int type){
-//		if(type == DetectedActivity.UNKNOWN)
-//			return "Unknown";
-//		else if(type == DetectedActivity.IN_VEHICLE)
-//			return "In Vehicle";
-//		else if(type == DetectedActivity.ON_BICYCLE)
-//			return "On Bicycle";
-//		else if(type == DetectedActivity.ON_FOOT)
-//			return "On Foot";
-//		else if(type == DetectedActivity.STILL)
-//			return "Still";
-//		else if(type == DetectedActivity.TILTING)
-//			return "Tilting";
-//		else
-//			return "";
-//	}
+	// Not used,  but can be used to convert the INTEGER to STRING that represents the Activity
+	private String getType(int type){
+		if(type == DetectedActivity.UNKNOWN)
+			return "Unknown";
+		else if(type == DetectedActivity.IN_VEHICLE)
+			return "In Vehicle";
+		else if(type == DetectedActivity.ON_BICYCLE)
+			return "On Bicycle";
+		else if(type == DetectedActivity.ON_FOOT)
+			return "On Foot";
+		else if(type == DetectedActivity.STILL)
+			return "Still";
+		else if(type == DetectedActivity.TILTING)
+			return "Tilting";
+		else
+			return "";
+	}
 
 }
